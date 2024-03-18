03477b9222b00591cc50b1096e9a21d7fb25a69f
Bradley Searle <brad@fuzzyhead.net>
2024-03-19 10:43:07 +1100

    Update Package.swift
    Signed-off-by: Bradley Searle <brad@fuzzyhead.net>

--- Package.swift
+++ Package.swift
@@ -3,18 +3,7 @@ import PackageDescription
 
 let package = Package(
   name: "SignalR-Swift",
-  products: [
-    .library(
-      name: "SignalRSwift", 
-      targets: [ "SignalRSwift" ]),
-  ],
-  dependencies: [
-      .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.6")
-  ],
-  targets: [
-    .target(
-      name: "SignalRSwift",
-      dependencies: [ "Starscream" ],
-      path: "SignalR-Swift"),
-  ]
+  products: [ .library(name: "SignalRSwift", targets: ["SignalRSwift"]) ],
+  dependencies: [ .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.6") ],
+  targets: [ .target(name: "SignalRSwift", dependencies: [ "Starscream" ], path: "SignalR-Swift") ]
 )
